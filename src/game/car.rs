5016917a9f2c978eede6f86217201e0de5da83d2
K4ugummi <stephan.schauerte@gmail.com>
2018-09-16 00:54:43 +0200

    Restructure

--- src/game/car.rs
+++ src/game/car.rs
@@ -1,45 +1,23 @@
-use nalgebra::{Point2, Vector2};
+extern crate glium;
+extern crate nalgebra;
 
-use time::Duration;
-
-pub(super) struct Scene {
-    cars: Vec<Car>,
-}
-
-impl Scene {
-    /// Make a new scene with a given number of cars.
-    pub(super) fn new(cars: usize) -> Scene {
-        Scene {
-            cars: (0..cars)
-                .map(|x| Car::new(Point2::new(x as f32, 0.), 1.0))
-                .collect(),
-        }
-    }
-
-    /// Update the scene based on the internal state and a given time step.
-    pub(super) fn run(&mut self, time_step: Duration) {
-        let time_step = (time_step.num_milliseconds() * 1_000) as f32;
-        //assert!(time_step > 0.);
-        for car in &mut self.cars {
-            car.run(time_step);
-        }
-    }
-}
+use self::nalgebra::{Point2, Vector2};
+use glium::Frame;
 
 pub(super) struct Car {
     pos: Point2<f32>,
-    rot: f32,
+    _rot: f32,
     vel: Vector2<f32>,
     force: Vector2<f32>,
     mass: f32,
 }
 
 impl Car {
-    pub(super) fn new(pos: Point2<f32>, mass: f32) -> Car {
+    pub fn new(pos: Point2<f32>, mass: f32) -> Car {
         assert!(mass > 0.);
         Car {
             pos,
-            rot: 0.,
+            _rot: 0.,
             vel: Vector2::new(0., 0.),
             force: Vector2::new(0., 0.),
             mass,
@@ -48,11 +26,15 @@ impl Car {
 
     /// Update the car position and velocity based on the internal car state for
     /// a given time step.
-    fn run(&mut self, time_step: f32) {
+    pub(super) fn run(&mut self, time_step: f32) {
         //assert!(time_step > 0.);
         self.pos += self.vel * time_step
             + self.force / (2. * self.mass) * time_step.powi(2);
         self.vel += self.force / self.mass * time_step;
+    }
+
+    pub(super) fn draw(&self, _target: &mut Frame) {
+        //target.draw();
     }
 }
 
---
+++ src/game/mod.rs
@@ -0,0 +1,44 @@
+mod car;
+mod scene;
+
+use self::scene::*;
+
+use glium::glutin::ControlFlow;
+use glium::Frame;
+
+use time::{Duration, PreciseTime};
+
+pub(crate) struct Game {
+    scene: Scene,
+    time: PreciseTime,
+}
+
+impl Game {
+    pub(crate) fn new() -> Game {
+        Game {
+            scene: Scene::new(3),
+            time: PreciseTime::now(),
+        }
+    }
+
+    fn update_time(&mut self) -> Duration {
+        let time_now = PreciseTime::now();
+        let time_step = self.time.to(time_now);
+        self.time = time_now;
+
+        time_step
+    }
+
+    pub(crate) fn run(&mut self) -> ControlFlow {
+        let time_step = self.update_time();
+        self.scene.run(time_step);
+        ControlFlow::Continue
+    }
+
+    pub fn draw(&self, target: &mut Frame) {
+        // TODO: Draw UI
+
+        // Draw game scene
+        self.scene.draw(target);
+    }
+}

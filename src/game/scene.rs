1a41bccf4ef8587e4588360d7f96c0b51e4285f8
K4ugummi <stephan.schauerte@gmail.com>
2018-09-27 13:28:53 +0200

    Finished first ultra basic rendering

--- src/game/scene.rs
+++ src/game/scene.rs
@@ -1,7 +1,6 @@
 use super::glium;
 use super::glium::Frame;
 use super::nalgebra::{Matrix4, Perspective3, Point2, Point3, Vector3};
-use super::rand::{thread_rng, Rng};
 use time::Duration;
 
 use super::car::Car;
@@ -13,7 +12,6 @@ pub(super) struct Scene {
 impl Scene {
     /// Make a new scene with a given number of cars.
     pub(super) fn new(cars: usize, display: &glium::Display) -> Scene {
-        let mut rng = thread_rng();
         Scene {
             cars: (0..cars)
                 .map(|x| {
@@ -21,9 +19,9 @@ impl Scene {
                         Point2::new(x as f32, 0.),
                         1.0,
                         {
-                            let r = 1.0f32; //rng.gen_range(0.0f32, 1.0f32);
-                            let g = 1.0f32; //rng.gen_range(0.0f32, 1.0f32);
-                            let b = 1.0f32; //rng.gen_range(0.0f32, 1.0f32);
+                            let r = 1.0f32;
+                            let g = 1.0f32;
+                            let b = 1.0f32;
                             Vector3::new(r, g, b)
                         },
                         display,
@@ -43,9 +41,9 @@ impl Scene {
 
     pub(super) fn draw(&self, target: &mut Frame) {
         let projection =
-            Perspective3::new(800. / 600., 45., 0.1, 100.).to_homogeneous();
+            Perspective3::new(800. / 600., 70., 0.1, 100.).unwrap();
         let view = Matrix4::look_at_rh(
-            &Point3::new(0., 0., 1.),
+            &Point3::new(0., 0., 50.),
             &Point3::new(0., 0., 0.),
             &Vector3::new(0., 1., 0.),
         );
--- src/game/shader.rs
+++ src/game/shader.rs
@@ -1,39 +1,33 @@
 use super::glium;
 
 pub(super) fn generate_program(display: &glium::Display) -> glium::Program {
-    let vertex_shader_src = r#"
-        #version 330 core
-        #extension GL_ARB_explicit_uniform_location : enable
-        #extension GL_ARB_separate_shader_objects : enable
+    let vertex_shader_src = "
+        #version 330
 
-        layout(location = 0) in vec2 position;
+        in vec2 position;
 
-        layout(location = 0) out vec4 vColor;
+        out vec4 vColor;
 
-        layout(location = 0) uniform mat4 uModel;
-        layout(location = 1) uniform mat4 uView;
-        layout(location = 2) uniform mat4 uProjection;
-        layout(location = 3) uniform vec3 uColor;
+        uniform mat4 uMVP;
+        uniform vec3 uColor;
 
         void main() {
             vColor = vec4(uColor, 1.);
-            gl_Position = uModel * uView * uProjection * vec4(position, 0., 1.);
+            gl_Position = uMVP * vec4(position, 0., 1.);
         }
-    "#;
+    ";
 
-    let fragment_shader_src = r#"
-        #version 330 core
-        #extension GL_ARB_explicit_uniform_location : enable
-        #extension GL_ARB_separate_shader_objects : enable
+    let fragment_shader_src = "
+        #version 330
 
-        layout(location = 0) in vec4 vColor;
+        in vec4 vColor;
 
-        layout(location = 0) out vec4 fragColor;
+        out vec4 fragColor;
 
         void main() {
             fragColor = vColor;
         }
-    "#;
+    ";
 
     let program = glium::Program::from_source(
         display,

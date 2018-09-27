6cc10d5301a8184298b93b6c60f25b702d265ce7
K4ugummi <stephan.schauerte@gmail.com>
2018-09-27 17:03:00 +0200

    Removed tests

--- src/game/shader.rs
+++ src/game/shader.rs
@@ -37,32 +37,3 @@ pub(super) fn generate_program(display: &glium::Display) -> glium::Program {
     );
     program.unwrap()
 }
-
-#[cfg(test)]
-mod test {
-    use super::{FRAGMENT_SHADER_SRC, VERTEX_SHADER_SRC};
-    use game::glium;
-    use glium::glutin;
-
-    #[test]
-    fn compile_shader() {
-        let version = glutin::GlRequest::Specific(glutin::Api::OpenGl, (3, 3));
-        let events_loop = glutin::EventsLoop::new();
-        let window = glutin::WindowBuilder::new().with_visibility(false);
-        let context = glutin::ContextBuilder::new()
-            .with_gl_debug_flag(true)
-            .with_gl(version);
-
-        let display =
-            glium::Display::new(window, context, &events_loop).unwrap();
-
-        let program = glium::Program::from_source(
-            &display,
-            VERTEX_SHADER_SRC,
-            FRAGMENT_SHADER_SRC,
-            None,
-        );
-
-        assert!(program.is_ok());
-    }
-}

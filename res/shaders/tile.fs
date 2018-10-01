1fe33e7acb77a66412e960c5c48e978fc2a65352
K4ugummi <stephan.schauerte@gmail.com>
2018-10-01 20:21:29 +0200

    Add license header to shader files

--- res/shaders/tile.fs
+++ res/shaders/tile.fs
@@ -1,3 +1,17 @@
+// This file is part of Carambolage.
+
+// Carambolage is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Carambolage is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with Carambolage.  If not, see <http://www.gnu.org/licenses/>.
 #version 330 core
 #extension GL_ARB_explicit_uniform_location : enable
 #extension GL_ARB_separate_shader_objects : enable

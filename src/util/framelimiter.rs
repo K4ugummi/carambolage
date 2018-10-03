f3547891db9c50143b469759bc2daa35cd3af7c8
K4ugummi <stephan.schauerte@gmail.com>
2018-10-03 16:27:17 +0200

    Add struct FrameLimiter

---
+++ src/util/framelimiter.rs
@@ -0,0 +1,36 @@
+use std::thread::sleep;
+use time::{Duration, PreciseTime};
+
+pub struct FrameLimiter {
+    time_per_frame: Duration,
+    delta_time: Duration,
+    time: PreciseTime,
+}
+
+impl FrameLimiter {
+    pub fn new(frame_rate: u32) -> FrameLimiter {
+        let time_per_frame = Duration::nanoseconds((1e9 / frame_rate as f64) as i64);
+        let time = PreciseTime::now();
+
+        FrameLimiter {
+            time_per_frame,
+            delta_time: Duration::nanoseconds(0),
+            time,
+        }
+    }
+
+    pub fn start(&mut self) -> Duration {
+        let now = PreciseTime::now();
+        self.delta_time = self.time.to(now);
+        self.time = now;
+
+        self.delta_time
+    }
+
+    pub fn stop(&mut self) {
+        if self.delta_time < self.time_per_frame {
+            let sleep_time = self.time_per_frame.checked_sub(&self.delta_time).unwrap().to_std().unwrap();
+            sleep(sleep_time);
+        }
+    }
+}
